991207cd890bab5bdc7755f9ebd9dd6ca95434d7
Alan D. Cabrera <adc@toolazydogs.com>
2012-08-12 09:53:53 -0700

    Removed extraneous watchers

--- tests/test_client.py
+++ tests/test_client.py
@@ -21,27 +21,10 @@ import time
 from nose.plugins.attrib import attr
 
 from toolazydogs import zookeeper
-from toolazydogs.zookeeper import  Persistent, AuthFailedError, Watcher, CREATOR_ALL_ACL, READ_ACL_UNSAFE, Ephemeral, PersistentSequential, EphemeralSequential
+from toolazydogs.zookeeper import  Persistent, AuthFailedError, CREATOR_ALL_ACL, READ_ACL_UNSAFE, Ephemeral, PersistentSequential, EphemeralSequential
 
 
 HOSTS = 'localhost'
-
-class Mine(Watcher):
-    def __init__(self):
-        pass
-
-    def session_connected(self, session_id, session_password, read_only):
-        pass
-
-    def session_expired(self, session_id):
-        pass
-
-    def connection_dropped(self):
-        pass
-
-    def connection_closed(self):
-        pass
-
 
 @attr('server')
 @attr('slow')
@@ -64,42 +47,6 @@ def test_auth():
         assert False, 'Allocation should have thrown an AuthFailedError'
     except AuthFailedError:
         pass
-
-
-class W(Watcher):
-    def __init__(self):
-        self.session_connected = 0
-        self.session_expired = 0
-        self.connection_dropped = 0
-        self.connection_closed = 0
-        self.node_created = 0
-        self.node_deleted = 0
-        self.data_changed = 0
-        self.children_changed = 0
-
-    def session_connected(self, session_id, session_password, read_only):
-        self.session_connected += 1
-
-    def session_expired(self, session_id):
-        self.session_expired += 1
-
-    def connection_dropped(self):
-        self.connection_dropped += 1
-
-    def connection_closed(self):
-        self.connection_closed += 1
-
-    def node_created(self, path):
-        self.node_created += 1
-
-    def node_deleted(self, path):
-        self.node_deleted += 1
-
-    def data_changed(self, path):
-        self.data_changed += 1
-
-    def children_changed(self, path):
-        self.children_changed += 1
 
 
 class Test(object):
@@ -227,7 +174,7 @@ class Test(object):
     def test_watch(self):
         hosts = HOSTS + self.chroot
 
-        z = zookeeper.allocate(hosts, watcher=)
+        z = zookeeper.allocate(hosts)
         random_data = _random_data()
         z.create('/pookie', CREATOR_ALL_ACL, Persistent(), data=random_data)
 
@@ -284,7 +231,7 @@ class Test(object):
     def test_transaction(self):
         hosts = HOSTS + self.chroot
 
-        z = zookeeper.allocate(hosts, watcher=Mine())
+        z = zookeeper.allocate(hosts)
 
         # this should fail because /bar does not exist
         z.create('/foo', CREATOR_ALL_ACL, Persistent())
